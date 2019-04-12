a09e29f1386cbad86bd9027826c90809d4de6cf1
Jonas Bernoulli <jonas@bernoul.li>
2019-04-12 09:19:00 +0200

    magit-reflog-mode: No longer derive from magit-log-mode
    While a reflog can be shown using "git log -g --abbrev-commit
    --pretty=online" a reflog is still rather different from a log.
    Usually a reflog is shown using "git reflog show", which is an
    alias for the mentioned log command and what Magit uses.
    
    This also fixes a bug.  Previously when showing the real log for the
    reflog entry at point the log arguments were picked up from the reflog
    arguments used in the reflog buffer, which is undesirable because that
    currently is just "-n256".

--- lisp/magit-log.el
+++ lisp/magit-log.el
@@ -1026,7 +1026,7 @@ Do not add this to a hook variable."
   (let ((magit-log-count 0))
     (magit-wash-sequence (apply-partially 'magit-log-wash-rev style
                                           (magit-abbrev-length)))
-    (if (derived-mode-p 'magit-log-mode)
+    (if (derived-mode-p 'magit-log-mode 'magit-reflog-mode)
         (when (eq magit-log-count (magit-log-get-commit-limit))
           (magit-insert-section (longer)
             (insert-text-button
@@ -1041,7 +1041,7 @@ Do not add this to a hook variable."
       (insert ?\n))))
 
 (cl-defun magit-log-wash-rev (style abbrev)
-  (when (derived-mode-p 'magit-log-mode)
+  (when (derived-mode-p 'magit-log-mode 'magit-reflog-mode)
     (cl-incf magit-log-count))
   (looking-at (pcase style
                 (`log        magit-log-heading-re)
@@ -1209,7 +1209,7 @@ exists mostly for backward compatibility reasons."
 (defun magit-log-maybe-update-revision-buffer (&optional _)
   "When moving in a log or cherry buffer, update the revision buffer.
 If there is no revision buffer in the same frame, then do nothing."
-  (when (derived-mode-p 'magit-log-mode 'magit-cherry-mode)
+  (when (derived-mode-p 'magit-log-mode 'magit-cherry-mode 'magit-reflog-mode)
     (magit-log-maybe-update-revision-buffer-1)))
 
 (defun magit-log-maybe-update-revision-buffer-1 ()
@@ -1233,7 +1233,7 @@ If there is no revision buffer in the same frame, then do nothing."
 (defun magit-log-maybe-update-blob-buffer (&optional _)
   "When moving in a log or cherry buffer, update the blob buffer.
 If there is no blob buffer in the same frame, then do nothing."
-  (when (derived-mode-p 'magit-log-mode 'magit-cherry-mode)
+  (when (derived-mode-p 'magit-log-mode 'magit-cherry-mode 'magit-reflog-mode)
     (magit-log-maybe-update-blob-buffer-1)))
 
 (defun magit-log-maybe-update-blob-buffer-1 ()
